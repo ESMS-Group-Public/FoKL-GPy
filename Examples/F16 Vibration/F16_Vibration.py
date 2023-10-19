c42d815d63fd1965cf85d67315dc97f51a3c441c
jakekrell <106118177+jakekrell@users.noreply.github.com>
2023-10-19 04:31:37 -0400

    Add files via upload

--- Examples/F16 Vibration/F16_Vibration.py
+++ Examples/F16 Vibration/F16_Vibration.py
@@ -15,12 +15,19 @@ warnings.filterwarnings("ignore", category=UserWarning)
 
 
 # ======================================================================================================================
+# User parameters for selecting dataset and controlling plots:
+
+
+dataset = [0,1,'max',3] # dataset to model . . .
+# . . . = [acceleration location (0,1,2), logical if modeling amplitude, 'min' or 'max' amplitude if applicable, . . .
+# . . . . . . number of sweeps to perform (i.e., ~number of neighbors to eliminate per max/min point)]
+res = 'full' # 1e10 # number of datapoints to look at (decrease for sake of example to decrease computation time)
+cbar_lim = [0, 4.8, 100] # limits of color bar in plots [min, max, number of discretizations]
+
+
+# ======================================================================================================================
 # Import experimental data from csv files and define custom functions for preparing experimental data:
 
-
-dataset = [0,1,'max'] # dataset to model . . .
-# . . . = [acceleration location (0,1,2), logical if modeling amplitude, 'min' or 'max' amplitude if applicable]
-res = 1e3 # number of datapoints to look at (decrease for sake of example to decrease computation time)
 
 dfs = []
 for n in [1,3,5,7]:
@@ -51,7 +58,8 @@ def clean_data_for_F16_example(dfs):
                 elif force_vector[new_index] < initial_peak_value:
                     return max_peak_index+1 # return next index, to ensure local max is not before true peak
         mm = find_first_peak_index(forceAmp[ii], force) # index to begin using data
-        mask = abs(force) <= forceAmp[ii] # mask datapoints exceeding nominal force amplitude
+
+        mask = np.ones(len(force), dtype=bool)
         mask[:mm+1] = False # mask datapoints before initial peak
         mm_end = (force != 0).nonzero()[0][-1]
         mask[mm_end+1::] = False  # mask datapoints after force is zero (i.e., vibration turned off)
@@ -62,16 +70,20 @@ def clean_data_for_F16_example(dfs):
         freq = np.linspace(15, 2, L)  # assuming 15Hz at first peak and 2Hz at vibration turned off
 
         # calculate phase of vibrational force to provide as an input
-        phase = np.arccos(force/forceAmp[ii]) # all within first and second quadrant
+        force_for_phase = force
+        force_for_phase[force > forceAmp[ii]] = forceAmp[ii] # set phase to within arccos range
+        force_for_phase[force < -forceAmp[ii]] = -forceAmp[ii]  # set phase to within arccos range
+        phase = np.arccos(force_for_phase/forceAmp[ii]) # all within first and second quadrant
         def true_phase_adjustment(phase_q12):
             # get forward indices of oscillation flip (assuming continuous changes):
             ff_out = []
-            diff0 = phase_q12[1]-phase_q12[0] # should be positive if phase_q12 starts jsut before local peak
-            for ff in range(2,len(phase_q12)):
-                diff1 = phase_q12[ff]-phase_q12[ff-1]
-                if np.sign(diff1) != np.sign(diff0): # if derivative of phase changes signs, then oscillation flips
-                    ff_out.append(ff)
-                diff0 = diff1
+            diff0 = -phase_q12[4] + 8*phase_q12[3] - 8*phase_q12[1] + phase_q12[0] # >0 b/c phase_q12[0] at/after peak
+            for ff in range(3, len(phase_q12) - 2):
+                if phase_q12[ff+1] != 0 and phase_q12[ff+1] != np.pi: # else force>forceAmp, so skip to next timestamp
+                    diff1 = -phase_q12[ff+2] + 8*phase_q12[ff+1] - 8*phase_q12[ff-1] + phase_q12[ff-2]
+                    if np.sign(diff1) != np.sign(diff0): # if derivative of phase changes signs, then oscillation flips
+                        ff_out.append(ff)
+                    diff0 = diff1
             phase_true = np.copy(phase_q12)
             for ff in range(0,len(ff_out)-1,2):
                 phase_true[ff_out[ff]:ff_out[ff+1]] = 2*np.pi-phase_q12[ff_out[ff]:ff_out[ff+1]]
@@ -80,7 +92,6 @@ def clean_data_for_F16_example(dfs):
             return phase_true
         phase = true_phase_adjustment(phase) # adjust phase values to all quadrants (assumes continuous changes)
 
-        # inputs.append([freq,force,phase])
         forceAmpVec = np.array([forceAmp[ii]]*L)
 
         inputs.append([freq,forceAmpVec,phase])
@@ -99,18 +110,20 @@ def clean_data_for_F16_example(dfs):
 
     return inputs_stacked, data_stacked
 
-def use_accelAmp_for_F16_example(inputs, data): # return data as Nx2 for [min,max] extrema
+def use_accelAmp_for_F16_example(inputs, data, sweeps): # return data as Nx2 for [min,max] extrema
     inputs_min = []
     inputs_max = []
     data_min = []
     data_max = []
     for a in [0,1,2]:
-        min_id = argrelextrema(data[a], np.less)  # indices of acceleration's local min's
-        max_id = argrelextrema(data[a], np.greater) # indices of acceleration's local max's
-
         # consider only local min's/max's that are negative/positive
-        min_id = min_id[0][data[a][min_id] < 0]
-        max_id = max_id[0][data[a][max_id] < 0]
+        min_id = np.array(np.where(data[a] < 0))
+        max_id = np.array(np.where(data[a] > 0))
+        for s in range(sweeps):
+            min_id_id = argrelextrema(data[a][tuple(min_id)], np.less) # indices of acceleration's local min's
+            max_id_id = argrelextrema(data[a][tuple(max_id)], np.greater) # indices of acceleration's local max's
+            min_id = min_id[0,min_id_id] # update indices
+            max_id = max_id[0,max_id_id] # update indices
 
         inputs_min.append(inputs[:, min_id])
         inputs_max.append(inputs[:, max_id])
@@ -127,11 +140,11 @@ def use_accelAmp_for_F16_example(inputs, data): # return data as Nx2 for [min,ma
 
 # clean raw data (NOT cleaning for machine learning purposes, but cleaning for focus of experiment/research)
 userinputs, userdata = clean_data_for_F16_example(dfs)
-inputs_min, inputs_max, data_min, data_max = use_accelAmp_for_F16_example(userinputs, userdata)
 
 # select specific dataset to model
 accel_id = dataset[0]
 if dataset[1] == 1:
+    inputs_min, inputs_max, data_min, data_max = use_accelAmp_for_F16_example(userinputs, userdata, dataset[3])
     if dataset[2] == 'min':
         userinputs = inputs_min[accel_id]
         userdata = data_min[accel_id]
@@ -142,8 +155,9 @@ elif dataset[1] == 0:
     userdata = userdata[accel_id]
 
 # for sake of example, shrink datasets to reduce computation time
-userinputs = userinputs[:, -round(res):]
-userdata = userdata[-round(res):]
+if res != 'full':
+    userinputs = userinputs[:, -round(res):]
+    userdata = userdata[-round(res):]
 
 
 # ======================================================================================================================
@@ -162,87 +176,38 @@ OutliersMethod = 'Method 1' # apply 'Method 1' for defining what constitutes an 
 betas, mtx, evs = model.fit(userinputs, userdata, p_train=p_train, CatchOutliers=CatchOutliers, OutliersMethod=OutliersMethod)
 
 
-# NOTES REGARDING ACCESSING MODEL ATTRIBUTES AFTER CALLING 'model.fit()' . . .
-
-# 'inputs' and 'data' have now been automatically formatted, cleaned, reduced to a train set, and stored as attributes.
-# > to access all relevant 'inputs' and 'data' after model.fit(), use:
-#   > model.inputs         == all normalized inputs w/o outliers (i.e., model.traininputs plus model.testinputs)
-#   > model.data           == all data w/o outliers (i.e., model.traindata plus model.testdata)
-# > to access other attributes, use:
-#   > model.rawinputs      == all normalized inputs w/ outliers == user's 'inputs' but normalized and formatted
-#   > model.rawdata        == all data w/ outliers              == user's 'data' but formatted
-#   > model.traininputs    == train set of model.inputs
-#   > model.traindata      == train set of model.data
-#   > model.testinputs     == test set of model.inputs
-#   > model.testdata       == test set of model.data
-#   > model.normalize      == [min, max] factors used to normalize user's 'inputs' to 0-1 scale of model.rawinputs
-#   > model.outliers       == indices removed from model.rawinputs and model.rawdata as outliers
-#   > model.trainlog       == indices of model.inputs used for model.traininputs
-#   > model.testlog        == indices of model.data used for model.traindata
-# > to access numpy versions of attributes related to 'inputs', use:
-#   > model.inputs_np      == model.inputs as a numpy array of timestamps x input variables
-#   > model.rawinputs_np   == model.rawinputs as a numpy array of timestamps x input variables
-#   > model.traininputs_np == model.traininputs as a numpy array of timestamps x input variables
-#   > model.testinputs_np  == model.testinputs as a numpy array of timestamps x input variables
-
-
 # ======================================================================================================================
 # Standard post-processing of FoKL model:
 
 
-meen, bounds, rmse = model.coverage3(model.inputs, model.data, model.draws, 1)
+meen, bounds, rmse = model.coverage3(model.inputs, model.data, model.draws)
+# meen, bounds, rmse = model.coverage3(model.inputs, model.data, model.draws, plot=1, bounds=1)
 
 
 # ======================================================================================================================
 # Additional user post-processing of FoKL model:
 
 
-# user plot controls:
-absAccel = 1 # 1 if plotting abs(accel), 0 if +/-
+# # SORTED:
+
+# plt.figure()
+# sort_id = np.squeeze(np.argsort(model.data, axis=0))
+# meen_sort = meen[sort_id]
+# bounds_sort = bounds[sort_id]
+# data_sort = model.data[sort_id]
+# plt.plot(meen_sort, 'b', linewidth=2)
+# plt.plot(bounds_sort[:, 0], 'k--')
+# plt.plot(bounds_sort[:, 1], 'k--')
+# plt.plot(data_sort, 'ro')
+# plt.title('model sorted by increasing data values')
+# plt.show()
 
 
-
-b=1
-
-# SORTED
-sort_id = np.argsort(meen1)
-meen1_sort = meen1[sort_id]
-bounds1_sort = np.transpose(np.array([bounds1[:, 0][sort_id], bounds1[:, 1][sort_id]]))
-data_sort = model1.data[sort_id]
-
-# COVERAGE3 PLOT
-# plt.plot(model1.data_validn, 'g.') # test
-plt.plot(model1.data, 'r.') # train
-plt.plot(meen1, 'b', linewidth=2)
-plt.plot(bounds1[:, 0], 'k--')
-plt.plot(bounds1[:, 1], 'k--')
-
-# WITHOUT ABS(ACCEL):
-plt.plot(data_sort, 'ro')
-plt.plot(meen1_sort, 'b', linewidth=2)
-plt.plot(bounds1_sort[:, 0], 'k--')
-plt.plot(bounds1_sort[:, 1], 'k--')
-plt.ylabel('Acceleration (m/s^2)')
-
-# WITH ABS(ACCEL):
-# plt.plot(np.abs(meen1_sort), 'b', linewidth=2)
-# plt.plot(np.abs(bounds1_sort[:, 0]), 'k--')
-# plt.plot(np.abs(bounds1_sort[:, 1]), 'k--')
-# plt.plot(np.abs(data_sort), 'ro')
-# plt.ylabel('|Acceleration| (m/s^2)')
-
-plt.show()
-
-# plt.cla()
-
-
-
-b=1
-
+# PLOT CONTOURS:
 
 # de-normalize inputs
-inputs_scale = model1.inputs_scale # [min,max] used to normalize each input
-inputs_np = model1.inputs_np
+inputs_scale = model.normalize # [min,max] used to normalize each input
+inputs_np = model.inputs_np
 M = np.shape(inputs_np)[1] # number of inputs
 inputs_denorm = []
 for ii in range(M):
@@ -253,7 +218,7 @@ for ii in range(M):
 # data for color map
 x = inputs_denorm[0] # frequency
 y = inputs_denorm[1] # force amplitude
-z = model1.data # predicted acceleration at location 1
+z = model.data # measured acceleration at location 1
 
 # define grid of force amp and freq pairs
 xi = np.linspace(x.min(), x.max(), 100)
@@ -263,19 +228,26 @@ xi, yi = np.meshgrid(xi, yi)
 # interpolate acceleration on the grid
 zi = griddata((x, y), z, (xi, yi), method='linear')
 zi = np.squeeze(zi)
+zi_model = griddata((x, y), meen, (xi, yi), method='linear')
+zi_model = np.squeeze(zi_model)
 
-if absAccel:
-    zi = np.abs(zi)
-    cbar_title = '|Acceleration| (m/s^2)'
-else:
-    cbar_title = 'Acceleration (m/s^2)'
-plt.contourf(xi, yi, zi, cmap='viridis')
+plt.figure()
+plt.subplot(1,2,1) # EXPERIMENTAL RESULTS:
+cbar_title = 'Acceleration (m/s^2)'
+plt.contourf(xi, yi, zi, cmap='viridis', levels=np.linspace(cbar_lim[0], cbar_lim[1], cbar_lim[2]))
 cbar = plt.colorbar()
 cbar.set_label(cbar_title)
-
 plt.xlabel('Frequency (Hz)')
 plt.ylabel('Force Amplitude (N)')
 plt.title('Induced Acceleration at Location 1, Measured Experimentally')
+plt.subplot(1,2,2) # FOKL MODEL OF RESULTS:
+cbar_title = 'Acceleration (m/s^2)'
+plt.contourf(xi, yi, zi_model, cmap='viridis', levels=np.linspace(cbar_lim[0], cbar_lim[1], cbar_lim[2]))
+cbar = plt.colorbar()
+cbar.set_label(cbar_title)
+plt.xlabel('Frequency (Hz)')
+plt.ylabel('Force Amplitude (N)')
+plt.title('Induced Acceleration at Location 1, Predicted with FoKL')
 plt.show()
 
 
